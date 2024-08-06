be63d50fd438df200ea727ef7ee0c66619afc63f
Artur Markouski <markouski.artur@student.ehu.lt>
2024-08-06 22:59:38 +0200

    feat: add solv tasks

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -3,6 +3,8 @@ package com.epam.m1.exceptions;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Complete the code, parse integers, calculate the sum of numbers in the WORDS, join strings with
@@ -19,13 +21,25 @@ public class ParseIntegers {
         Iterator<String> words = WORDS.iterator();
         int sum = 0;
         String justWords = "";
+        Pattern pattern  = Pattern.compile("\\d+");
+
         while (words.hasNext()) {
             String next = words.next();
-            int number = Integer.parseInt(next);
-            // todo: complete it
+            Matcher matcher = pattern.matcher(next);
+            if (matcher.find()){
+                try {
+                    int number = Integer.parseInt(next);
+                    sum += number;
+                }catch (NumberFormatException e){
+                    System.out.println();
+                }
+            }else {
+                justWords += next;
+                justWords += " ";
+            }
         }
         System.out.println("Sum is " + sum);
-        System.out.println("Just words:" + justWords);
+        System.out.println("Just words: " + justWords);
     }
 }
 
